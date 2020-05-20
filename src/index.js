32d14125407eae12f0bf70b0a03b5b8d5eb5bb7c
Dangeecko <poidydaniel@gmail.com>
2020-05-20 23:34:32 +0000

    Mise en place du fichier js de base et de la feuille de style

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as serviceWorker from './serviceWorker'
+
+import './style.css'
+
+class Main extends React.Component {
+  render () {
+    return (
+      <div className='container'>
+        <h1 className='title'>React Movie Search</h1>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
